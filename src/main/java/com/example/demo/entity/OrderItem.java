7a817ca8b996d25db053c873030f6bd0002623f8
teacher <teacher@BE-403->
2022-09-05 20:56:53 +0800

    psi

--- src/main/java/com/example/demo/entity/OrderItem.java
+++ src/main/java/com/example/demo/entity/OrderItem.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,8 +17,48 @@ public class OrderItem {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column
+	private Integer amount; // 訂購數量
+	
 	@JoinColumn(name = "order_id")  // 訂單序號(外鍵)
 	@ManyToOne
 	private Order order;
+	
+	@JoinColumn(name = "product_id")  // 商品序號(外鍵)
+	@ManyToOne
+	private Product product;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
 	
 }
