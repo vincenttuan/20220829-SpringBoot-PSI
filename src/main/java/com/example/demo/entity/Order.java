200ff67f5b393adf0f18f56f1755d4fc34eef1f2
teacher <teacher@BE-403->
2022-09-05 20:46:14 +0800

    psi

---
+++ src/main/java/com/example/demo/entity/Order.java
@@ -0,0 +1,35 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "order")
+public class Order {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date date; // 訂單日期
+	
+	@JoinColumn(name = "customer_id")
+	@ManyToOne
+	Customer customer;
+	
+	
+}
