bd70a99d313e942c237798468fa27fc310cdb30f
teacher <teacher@BE-403->
2022-09-05 20:03:09 +0800

    psi

--- src/main/java/com/example/demo/entity/Purchase.java
+++ src/main/java/com/example/demo/entity/Purchase.java
@@ -1,6 +1,8 @@
 package com.example.demo.entity;
 
 import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -34,5 +38,51 @@ public class Purchase {
 	@JoinColumn(name = "employee_id") // 員工序號(外鍵)
 	@ManyToOne
 	private Employee employee;
+	
+	@OneToMany(mappedBy = "purchase")
+	@OrderBy("id ASC")
+	private Set<PurchaseItem> purchaseItems = new LinkedHashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Supplier getSupplier() {
+		return supplier;
+	}
+
+	public void setSupplier(Supplier supplier) {
+		this.supplier = supplier;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public Set<PurchaseItem> getPurchaseItems() {
+		return purchaseItems;
+	}
+
+	public void setPurchaseItems(Set<PurchaseItem> purchaseItems) {
+		this.purchaseItems = purchaseItems;
+	}
+	
+	
 	
 }
---
+++ src/main/java/com/example/demo/entity/PurchaseItem.java
@@ -0,0 +1,26 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "purchase_item")
+public class PurchaseItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private Integer amount; // 採購數量
+	
+	@JoinColumn(name = "purchase_id")
+	@ManyToOne
+	private Purchase purchase;
+	
+}
