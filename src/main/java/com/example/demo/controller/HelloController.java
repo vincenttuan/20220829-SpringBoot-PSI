b36a199ef6d405a908854d1fffcc15f10a41c96c
teacher <teacher@BE-403->
2022-09-09 20:52:20 +0800

    psi

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,8 +12,59 @@ public class HelloController {
 	
 	// 執行網址: /psi/hello/
 	@RequestMapping("/") 
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("time", new Date());
 		return "hello";
 	}
 	
 }
+
+class User {
+	int id;
+	String name;
+	int age;
+	Date birth;
+	
+	public User() {
+		
+	}
+	
+	public User(int id, String name, int age, Date birth) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.birth = birth;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", age=" + age + ", birth=" + birth + "]";
+	}
+	
+	
+}
+
