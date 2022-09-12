9dd3734dfb67d4036187429979539c653d322b60
teacher <teacher@BE-403->
2022-09-12 19:42:08 +0800

    psi

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -1,15 +1,24 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.entity.Department;
+import com.example.demo.repository.DepartmentRepository;
 
 @Controller
 @RequestMapping("/department")
 public class DepartmentController {
 	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+	
 	@RequestMapping("/")
 	public String index(Model model) {
+		model.addAttribute("department", new Department());
+		model.addAttribute("departments", departmentRepository.findAll());
 		return "department"; 
 	}
 	
