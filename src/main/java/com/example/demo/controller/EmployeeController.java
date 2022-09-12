d1470cb4eb22a3de07ec518f691e668bcb45a335
teacher <teacher@BE-403->
2022-09-12 21:26:38 +0800

    psi

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.entity.Department;
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.DepartmentRepository;
+import com.example.demo.repository.EmployeeRepository;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("employee", new Employee());
+		model.addAttribute("employees", employeeRepository.findAll());
+		model.addAttribute("departments", departmentRepository.findAll());
+		return "employee"; 
+	}
+	
+	
+}
