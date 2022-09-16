aea96157d587cc5f9a96b36fb454e6453dee75b1
teacher <teacher@BE-403->
2022-09-16 20:52:34 +0800

    psi

---
+++ src/main/java/com/example/demo/service/LineNotifyService.java
@@ -0,0 +1,46 @@
+package com.example.demo.service;
+
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.example.demo.entity.Product;
+
+public class LineNotifyService {
+	
+	public void send(Product product) throws Exception {
+        // 1. 要發送的資料
+        String message = String.format("商品名稱: %s\n商品特價: %d", product.getName(), product.getPrice());
+        // 2. 存取權杖(也稱為:授權 Token)
+        String token = "存取權杖";
+        // 3. Line Notify 的發送位置
+        String lineNotifyUrl = "https://notify-api.line.me/api/notify";
+        // 4. 發送前設定 -------------------------------------------------------------------------
+        // 發送文字
+        byte[] postData = ("message=" + message).getBytes("UTF-8");
+        // 發送文字 + 縮略圖 + 網路圖片
+        // String picurl = "https://image.cache.u-car.com.tw/articleimage_1091049.jpg";
+        // byte[] postData = ("message=" + message + "&stickerPackageId=1&stickerId=113&imageThumbnail=" + picurl + "&imageFullsize=" + picurl).getBytes("UTF-8");
+
+        int postDataLength = postData.length;
+        URL url = new URL(lineNotifyUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Authorization", "Bearer " + token); // 一定要加
+        conn.setRequestProperty("charset", "utf-8"); // 可以不用加
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded"); // 可以不用加
+        conn.setRequestProperty("Content-Length", Integer.toString(postDataLength)); // 可以不用加
+        conn.setUseCaches(false);
+        // 5. 訊息發送 ---------------------------------------------------------------------------
+        try (DataOutputStream wr = new DataOutputStream(conn.getOutputStream())) {
+            wr.write(postData);
+            wr.flush();
+        }
+        // 6. 回應資料 ---------------------------------------------------------------------------
+        if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+            int statusCode = conn.getResponseCode();
+            System.out.println(statusCode);
+        }
+    }
+}
