c5375c83a8664ccc07d55678eff621e2b8c34e18
teacher <teacher@BE-403->
2022-09-23 19:04:10 +0800

    psi

---
+++ src/main/java/com/example/demo/controller/InventoryController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/inventory")
+public class InventoryController {
+	
+	public String index(Model model) {
+		return "inventory";
+	}
+	
+}
