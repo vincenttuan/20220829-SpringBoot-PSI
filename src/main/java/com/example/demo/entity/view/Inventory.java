1561124ad7143a6853f99ddea48031d0c73d9dc6
teacher <teacher@BE-403->
2022-09-23 19:09:24 +0800

    psi

---
+++ src/main/java/com/example/demo/entity/view/Inventory.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity.view;
+
+// 即時庫存
+public interface Inventory {
+	Long getId(); // 序號
+	String getName(); // 商品名稱
+	Integer getCost(); // 商品成本
+	Integer getPrice(); // 商品售價
+	Integer getAmount1(); // 採購數量
+	Integer getAmount2(); // 銷售數量
+}
