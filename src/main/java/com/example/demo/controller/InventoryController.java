2e0bee470665b9f652f5c94aedf276e250c01e8b
teacher <teacher@BE-403->
2022-09-23 19:19:33 +0800

    psi

--- src/main/java/com/example/demo/controller/InventoryController.java
+++ src/main/java/com/example/demo/controller/InventoryController.java
@@ -1,15 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.repository.ProductRepository;
 
 @Controller
 @RequestMapping("/inventory")
 public class InventoryController {
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@RequestMapping("/")
 	public String index(Model model) {
+		model.addAttribute("inventories", productRepository.queryInventory());
 		return "inventory";
 	}
 	
