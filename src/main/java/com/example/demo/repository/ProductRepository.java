c047e5b10c0d3d2bc957e820ba99030635bf38b3
teacher <teacher@BE-403->
2022-09-23 21:27:34 +0800

    psi

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Product;
 import com.example.demo.entity.view.Inventory;
+import com.example.demo.entity.view.ProductSales;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
@@ -24,6 +25,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 			+ "from product p where p.id=:id ")
 	Inventory findInventoryById(Long id);
 	
-	
+	@Query(nativeQuery = true, value = "select p.id, p.name, sum(o.amount) as total "
+			+ "from order_item o, product p "
+			+ "where o.product_id = p.id "
+			+ "group by p.id, p.name ")
+	List<ProductSales> queryProductSales(); 
 	
 }
