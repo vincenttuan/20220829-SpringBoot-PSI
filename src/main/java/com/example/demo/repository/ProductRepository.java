1fd40403b72f1bfc51c2ebbc9a3caabb8e28fe88
teacher <teacher@BE-403->
2022-09-23 19:17:29 +0800

    psi

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -1,11 +1,21 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Product;
+import com.example.demo.entity.view.Inventory;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	@Query(nativeQuery = true, value = "select p.id, p.name, p.cost, p.price, "
+			+ "(select sum(amount) from purchase_item where product_id = p.id limit 1) as amount1, "
+			+ "(select sum(amount) from order_item where product_id = p.id limit 1) as amount2 "
+			+ "from product p")
+	List<Inventory> queryInventory();
 	
 }
